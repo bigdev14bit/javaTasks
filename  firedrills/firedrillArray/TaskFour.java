d4a8181a515c51181d2dba913ae4995b2967445e
bigdev14bit <devbig14@gmail.com>
2026-05-04 18:43:39 +0100

    Added new files

---
+++  firedrills/firedrillArray/TaskFour.java
@@ -0,0 +1,27 @@
+public class TaskOne {
+  public static void main(String[] args) {
+
+  java.util.Scanner inputCollector = new java.util.Scanner(System.in);
+
+  int[] totalScore = new int[10];
+
+  int count = 0;
+
+  for(int index = 0; index < totalScore.length; index++) {
+
+    System.out.print("Enter Score: ");
+    int userInput = inputCollector.nextInt();
+
+    totalScore[index] = userInput;
+
+    System.out.println(totalScore);
+
+
+  }
+
+  for(int indexx = 0; indexx < 10; indexx++) {
+    System.out.print(indexx);
+  }
+  System.out.println();
+ }
+}
